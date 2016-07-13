a3450d282960b888d13caadaa6f080861923ad40
Philipe Luna <philipe.luna@dce.ufpb.br>
2016-07-13 10:58:59 -0300

    criação das primeiras telas

---
+++ app/src/main/java/com/example/philipe/easytransactionsapplication/OperacoesDaContaActivity.java
@@ -0,0 +1,13 @@
+package com.example.philipe.easytransactionsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OperacoesDaContaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operacoes_da_conta);
+    }
+}
